5691c128f781dcbe912679a74166d7c10bc9190b
criativity <criativity0@gmail.com>
2022-12-13 23:21:37 -0300

    Instalação do tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./GreenFirstGoal/Views/Shared/FirstGoalBattle.cshtml",
+        "./GreenFirstGoal/Views/Home/*.cshtml",
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
